52194426c8c40d68ab95ee2d4e93e1d7911a3d68
noriakis <nori@hgc.jp>
2023-11-06 10:35:40 +0900

    plotSubNetMatrix

---
+++ R/obtainSubNetMatrix.R
@@ -0,0 +1,24 @@
+#' obtainSubNetMatrix
+#' 
+#' @param df the results of `globalStrucValues` (specifying the bn object)
+obtainSubNetMatrix <- function(df, spe) {
+	
+	
+	logc <- spe@assays@data$logcounts
+
+	all_nodes <- unique(c(df$from, df$to))
+	mat <- do.call(rbind, lapply(all_nodes, function(x) {
+	    subset_graph <- df |> filter(
+	        from %in% x | to %in% x
+	    )
+	    subset_nodes <- unique(c(subset_graph$from, subset_graph$to))
+	    in_net <- logc[subset_nodes, ]
+	    if (is.vector(in_net)) {
+	    	return(in_net)
+	    } else {
+    	    return(apply(in_net, 2, sum))
+	    }
+	}))
+	row.names(mat) <- paste0(all_nodes, "_net")
+	return(mat)
+}
