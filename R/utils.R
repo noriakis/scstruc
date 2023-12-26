67ea711dc0564fb5e8ef495354dba08c7be1db91
noriakis <nori@hgc.jp>
2023-12-26 15:49:51 +0900

    bnfit to igraph function

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,31 @@
 
 #' shdmat
+#' 
+#' @export
 shdmat <- function(netlist) {
     sapply(netlist, function(x) sapply(netlist, function(y) bnlearn::shd(x,y)))
 }
 
+
+#' @export
 convert_to_tbl_graph <- function(bn) {
     bn |> 
     bnlearn::as.igraph() |>
     tidygraph::as_tbl_graph()
 }
+
+
+#' @export
+bn.fit_to_igraph <- function(graph) {
+	igraph::graph_from_data_frame(do.call(rbind, lapply(names(graph), function(i) {
+	    tmp <- graph[[i]]
+	    if (length(tmp$coefficients)>1) {
+	        tmp <- data.frame(tmp$coefficients[2:length(tmp$coefficients)]) %>%
+	        `colnames<-`(c("coef"))
+	        tmp[["from"]] <- row.names(tmp)
+	        tmp[["to"]] <- rep(i, nrow(tmp))
+	        return(tmp[,c("from","to","coef")])
+	    }
+	})))
+}
+
