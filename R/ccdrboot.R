08309fae8f806daa47a56ef7a52b6714c8debe19
noriakis <31095487+noriakis@users.noreply.github.com>
2024-01-05 22:01:28 +0900

    ccdr.boot

---
+++ R/ccdrboot.R
@@ -0,0 +1,71 @@
+#' ccdr.boot
+#' 
+#' Bootstrap-based arc strength calculation based on CCDr algorithm
+#' 
+#' @param data
+#' @param lambda.length passed to generate.lambdas
+#' @param alpha passed to ccdr.run
+#' @param gamma passed to ccdr.run
+#' @export
+ccdr.boot <- function(data, R=200, m=nrow(data), lambdas.length=20, alpha=2, gamma=2) {
+    nodes = names(data)
+    ## Use same lambda for all replicate (probably not needed)
+    ## Set alpha to be sufficiently large
+    lambdas <- sparsebnUtils::generate.lambdas(lambda.max=sqrt(nrow(data)), lambdas.ratio=1e-2,
+        lambdas.length=lambdas.length, scale="log")
+    perRun <- list()
+    for (r in seq_len(R)) {
+        resampling = sample(nrow(data), m, replace = TRUE)
+        # generate the r-th bootstrap sample.
+        replicate = data[resampling, , drop = FALSE]
+        dat <- sparsebnUtils::sparsebnData(replicate, type = "continuous")
+        run <- ccdr.run(data = dat, lambdas=lambdas,
+            alpha=alpha, gamma=gamma, verbose=FALSE)
+        perRun[[r]] <- run
+    }
+    all_lambdas <- Reduce(intersect, lapply(perRun, function(pr) {unlist(lapply(pr, function(x) x$lambda))}))
+    probs <- lapply(which(lambdas %in% all_lambdas), function(l) {
+        prob = matrix(0, ncol = ncol(data), nrow = ncol(data))
+        for (pr in perRun) {
+            tmp <- pr[[l]]
+            tmpbn <- sparsebnUtils::to_igraph(tmp$edges)
+            tmpadj <- igraph::get.adjacency(tmpbn, sparse=FALSE)
+            for (i in seq_len(ncol(data))) {
+                for (j in seq_len(ncol(data))) {
+                    if (tmpadj[i,j] == 1) {
+                        prob[i,j] <- prob[i,j] + 1
+                    }
+                }
+            }
+        }
+        prob / R
+    })
+    names(probs) <- as.character(lambdas[lambdas %in% all_lambdas])
+    ## Strength and direction computing
+    res <- lapply(names(probs), function(p) {
+        amat <- probs[[p]]
+        ret <- NULL
+        for (i in seq_len(ncol(data))) {
+            for (j in seq_len(ncol(data))) {
+                if (i==j) {
+                    next
+                }
+                s <- amat[i, j] + amat[j, i]
+                if (s==0) {d <- 0} else {
+                    d <- amat[i,j] / s
+                }
+                if (d < 0) {d <- 0}
+                if (d > 1) {d <- 1}
+                if (s < 0) {s <- 0}
+                if (s > 1) {s <- 1}
+                ret <- rbind(ret, c(nodes[i], nodes[j], s, d))
+            }
+        }
+        ret <- data.frame(ret) %>% `colnames<-`(c("from","to","strength","direction"))
+        ret$strength <- as.numeric(ret$strength)
+        ret$direction <- as.numeric(ret$direction)
+        ret
+    })
+    names(res) <- as.character(lambdas[lambdas %in% all_lambdas])
+    res
+}
---
+++ R/gen.R
@@ -0,0 +1,16 @@
+#' gen
+#' random.graph based on gene node name
+#' @param method ic-dag, melancon or ordered. default to melancon.
+#' @param params pass to random.graph
+#' @export
+#' @return bn object and bn.fit object
+gen <- function(n, data, method="melancon", params=list()) {
+    nodename <- sample(colnames(data), n, replace=FALSE)
+
+    params[["method"]] <- method
+    params[["nodes"]] <- nodename
+    
+    rdbn <- do.call(bnlearn::random.graph, params)
+    fitted <- bn.fit(rdbn, data[, nodename])
+    return(list("net"=rdbn, "fitted"=fitted))
+}
