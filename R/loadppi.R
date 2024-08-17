195b49dbaec462b79368d62e23b03e1516c7db45
noriakis <nori@hgc.jp>
2024-08-17 19:10:52 +0900

    ppi hs

--- R/loadppi.R
+++ R/loadppi.R
@@ -7,16 +7,29 @@
 #' Szklarczyk D, Gable AL, Lyon D, Junge A, Wyder S, Huerta-Cepas J, Simonovic M, Doncheva NT, Morris JH, Bork P, Jensen LJ, Mering CV. STRING v11: protein-protein association networks with increased coverage, supporting functional discovery in genome-wide experimental datasets. Nucleic Acids Res. 2019 Jan 8;47(D1):D607-D613. doi: 10.1093/nar/gky1131. PMID: 30476243; PMCID: PMC6323986.
 #' 
 #' @export
-#' @param org org, mm or hsa
+#' @param org organism, mm or hsa
 #' @param database string or biogrid
 #' @examples
 #' db <- loadppi()
 loadppi <- function(org="mm", database="string") {
     # if(is.null(cache$table)) {
     if (database=="string") {
-      tb = readRDS(system.file("extdata", "ppi_mm_900.rds", package = "scstruc"))
+      if (org == "mm") {
+        tb = readRDS(system.file("extdata", "ppi_mm_900.rds", package = "scstruc"))
+      } else if (org == "hsa") {
+        tb = readRDS(system.file("extdata", "ppi_hs_900.rds", package = "scstruc"))
+      } else {
+        stop("mm or hsa should be specified to database")
+      }
     } else if (database=="biogrid") {
-      tb = readRDS(system.file("extdata", "ppi_mm_biogrid.rds", package = "scstruc"))
+      if (org == "mm") {
+        tb = readRDS(system.file("extdata", "ppi_mm_biogrid.rds", package = "scstruc"))
+
+      } else if (org=="hsa") {
+        return(NULL)
+      } else {
+        stop("mm or hsa should be specified to database")
+      }
     } else {
       stop("`database` argument should be string or biogrid")
     }
@@ -83,4 +96,25 @@ intersectPpi <- function(edge_names, org="mm", return_net=FALSE) {
     df$proportion <- as.numeric(df$proportion)
     row.names(df) <- seq_len(nrow(df))
     return(df)
+}
+
+#' @title returnKEGGedges
+#' @description return directed igraph object
+#' @param pathID pathway ID
+#' @param args arguments of ggkegg::pathway
+#' @details The function uses ggkegg to obtain and parse KEGG PATHWAY information
+#' using KEGG RESTful API.  There should be type `gene` in the node data of pathway
+#' this returns the directed relationship described in the pathway
+#' @export
+#' @importFrom dplyr filter select
+returnKEGGedges <- function(pathID, args) {
+    if (!requireNamespace("ggkegg")) {
+        stop("Needs ggkegg. Please install from Bioconductor.")
+    }
+    args[["pid"]] <- pathID
+    pway <- do.call(ggkegg::pathway, args)
+    gs <- pway %N>% dplyr::filter(type=="gene")
+    nname <- gs %N>% data.frame() %>% dplyr::pull(graphics_name) %>% strsplit(",") %>% sapply("[",1)
+    gs %E>% mutate(fromn = nname[from], ton=nname[to]) %>% data.frame() %>% mutate(from=fromn, to=ton) %>%
+        dplyr::select(-fromn) %>% dplyr::select(-ton) %>% igraph::graph_from_data_frame(directed=TRUE)
 }
