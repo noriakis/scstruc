fd51be6d72ef338ce8df48fd96b4b5cfa17196ee
noriakis <nori@hgc.jp>
2024-09-27 13:13:53 +0900

    ccdr separation

--- R/getStruc.R
+++ R/getStruc.R
@@ -6,44 +6,7 @@
         cat_subtle("Algorithm: ", algorithm, " selected\n")        
     }
     if (algorithm=="ccdr") {
-        if (is.null(algorithm.args[["bestScore"]])) {
-            pickBest <- TRUE
-        } else {
-            if (!algorithm.args[["bestScore"]]) {
-                pickBest <- FALSE
-            } else {
-                pickBest <- TRUE
-            }
-        }
-        algorithm.args[["bestScore"]] <- NULL
-        dat <- sparsebnUtils::sparsebnData(input %>% as.matrix(), type = "continuous")
-        algorithm.args[["data"]] <- dat
-        if (is.null(algorithm.args[["lambdas.length"]]) & is.null(algorithm.args[["lambdas"]])) {
-            cat_subtle("Setting `lambdas.length` to 10\n")
-            algorithm.args[["lambdas.length"]] = 10
-        }
-        ccdr.res <- do.call(ccdrAlgorithm::ccdr.run, algorithm.args)
-        bn.res <- lapply(seq_along(ccdr.res), function(x) {
-            sparsebnUtils::to_bn(ccdr.res[[x]]$edges)
-        })
-        names(bn.res) <- lapply(seq_along(ccdr.res), function(x) {
-            ccdr.res[[x]]$lambda
-        }) %>% unlist()
-
-        if (pickBest) {
-            cat_subtle("Returning best scored network only, specify algorithm.args=list(bestScore=FALSE) to return all the network\n")
-            bestind <- names(which.max(lapply(bn.res, function(tt) {
-                score(tt,  data.frame(input, check.names=FALSE)[names(tt$nodes), ])
-            })))
-            if (length(bestind)==1) {
-                return(bn.res[[bestind]])
-            } else {
-                return(bn.res[bestind])
-            }
-        } else {
-            cat_subtle("Returning the bn per lambda from result of ccdr.run\n")
-            return(bn.res)                
-        }
+        return(ccdr(input, algorithm, algorithm.args, verbose))
     } else if (algorithm=="ccdr.boot") {
         cat_subtle("Returning the list of bn.strength\n")
         algorithm.args[["data"]] <- input
