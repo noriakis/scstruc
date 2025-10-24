38becfdef28bfcfded92fd1fe2944cb481a1f47d
noriakis <nori@hgc.jp>
2025-10-24 12:03:31 +0900

    use of omnipath

---
+++ R/extractOmnipath.R
@@ -0,0 +1,53 @@
+#' @title extractDAGfromOmnipath
+#' This function takes the pathway name in SignaLink and
+#' 1. Subset the pathway-related genes in dorothea interaction and remove duplicates
+#' 2. Identify and remove minimal feedback arc set (make DAG)
+#' 3. Takes the largest components in the graph
+#' @import OmnipathR
+#' @importFrom dplyr %>%
+#' @importFrom igraph delete.edges feedback_arc_set
+#' @import tidygraph
+#' @examples
+#' extractDAGfromOmnipath("TGF")
+#' @export
+extractDAGfromOmnipath <- function(pathway_name, target_source="AND") {
+    doro <- dorothea()
+    network_slk_pw <- annotated_network(doro, 'SignaLink_pathway')
+    
+    pathway_list <- unique(network_slk_pw$pathway_target)
+    pathway_list <- pathway_list[!is.na(pathway_list)]
+    
+    if (!(pathway_name %in% pathway_list)) {
+        stop("No SignaLink pathway of that name")
+    }
+    
+    if (target_source=="OR") {
+        sub <- network_slk_pw %>%
+            filter(pathway_target==pathway_name | pathway_source==pathway_name) %>%
+            filter(dorothea_level %in% c("A"))
+    } else if (target_source == "AND") {
+        sub <- network_slk_pw %>%
+            filter(pathway_target==pathway_name & pathway_source==pathway_name) %>%
+            filter(dorothea_level %in% c("A"))       
+    } else {
+        stop("target_source should be OR or AND")
+    }
+    
+    graph <- sub[,c("source_genesymbol","target_genesymbol")] %>%
+        `colnames<-`(c("from","to"))
+    
+    graph.nodup <- graph[!duplicated(graph[, c("from","to")]),]
+    ig <- igraph::graph_from_data_frame(graph.nodup)
+    ig2 <- delete.edges(ig,  feedback_arc_set(ig))
+
+    if (!is_dag(ig2)) {stop("The resulting graph is not DAG")}
+    
+    comp <- to_components(ig2)
+    maxind <- which.max(unlist(lapply(comp, function(XX) {
+        dm <- XX %N>% data.frame(); dim(dm)[1]})))
+    ig3 <- comp[[maxind]]
+    
+    return(ig3)
+    
+}
+
